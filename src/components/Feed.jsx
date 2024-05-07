bf0c7cdda3875293110391f8e4b86fa48f4c3321
prasannakiit <2105980@kiit.ac.in>
2024-05-07 13:46:26 +0530

    mini profile section(might discard later)

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,5 @@
 import MiniProfile from './MiniProfile';
 import Posts from './Posts';
-
 export default function Feed() {
   return (
     <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
@@ -11,7 +10,10 @@ export default function Feed() {
 
       {/* MiniProfile (Right) */}
       <section className='hidden md:inline-grid md:col-span-1'>
-        <MiniProfile />
+        
+        <div className='fixed w-[380px]'>
+          <MiniProfile />
+        </div>
       </section>
     </main>
   );
